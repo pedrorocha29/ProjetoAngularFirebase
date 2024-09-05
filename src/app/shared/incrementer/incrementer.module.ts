98fef8714089c991e1cca71f8d57157678596620
pedrorocha29 <162391720+pedrorocha29@users.noreply.github.com>
2024-09-05 23:29:14 +0000

    aula 05/09/2024

---
+++ src/app/shared/incrementer/incrementer.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IncrementerComponent } from './incrementer.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [IncrementerComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+
+  exports: [
+    IncrementerComponent
+
+  ]
+})
+export class IncrementerModule { }
