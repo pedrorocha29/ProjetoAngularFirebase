d2147a9b7246291b9afc0730a66672cb3f296539
pedrorocha29 <162391720+pedrorocha29@users.noreply.github.com>
2024-09-06 23:11:22 +0000

    main restaurada

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import { IncrementerModule } from '../shared/incrementer/incrementer.module';
 
 
 @NgModule({
@@ -14,7 +13,6 @@ import { IncrementerModule } from '../shared/incrementer/incrementer.module';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
-    IncrementerModule
   ],
   declarations: [HomePage]
 })
