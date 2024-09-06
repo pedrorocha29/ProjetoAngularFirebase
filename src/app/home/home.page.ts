342f26d4385d8620cd8d99511a9681760da30ebe
pedrorocha29 <162391720+pedrorocha29@users.noreply.github.com>
2024-09-06 23:51:33 +0000

    api pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { AuthenticateService } from '../services/auth.service';
 import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
+import { ApiService } from '../shared/services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,9 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+  constructor(
+    public apiService: ApiService
+  ){ 
+    this.apiService.getPokemons();
+  }
 }
